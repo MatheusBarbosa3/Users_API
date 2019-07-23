53079327a7c8ef1d9be4d02bdc36d1f144ee9e2e
MatheusBarbosa3 <matheusbdos31@gmail.com>
2019-07-23 17:28:10 -0300

    Config appropriate status code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
     const token_header = req.headers.auth;
-    if (!token_header) return res.send({ message: 'Token not send' });
+    if (!token_header) return res.status(401).send({ message: 'Token not send' });
 
     jwt.verify(token_header, 'password123', (err, decoded) => {
         if (err) return res.send({ message: 'Invalid token' });
--- routes/users.js
+++ routes/users.js
@@ -14,44 +14,44 @@ router.get('/', async (req, res) => {
         return res.send(users);
 
     } catch (error) {
-        return res.send({ message: 'Error in query of users' });
+        return res.status(500).send({ message: 'Error in query of users' });
     }
 });
 
 router.post('/create', async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || !password) return res.send({ message: 'No data sufficient' });
+    if (!email || !password) return res.status(400).send({ message: 'No data sufficient' });
 
     try {
-        if (await Users.findOne({ email })) return res.send({ message: 'User already registered' });
+        if (await Users.findOne({ email })) return res.status(400).send({ message: 'User already registered' });
 
         const user = await Users.create(req.body);
         user.password = undefined;
-        return res.send({ user, token: createTokenUser(user.id) });
+        return res.status(201).send({ user, token: createTokenUser(user.id) });
 
     } catch (error) {
-        return res.send({ message: 'Error fetching users' });
+        return res.status(500).send({ message: 'Error fetching users' });
     }
 });
 
 router.post('/auth', async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || !password) return res.send({ message: 'No data sufficient' });
+    if (!email || !password) return res.status(400).send({ message: 'No data sufficient' });
 
     try {
         const user = await Users.findOne({ email }).select('+password');
-        if (!user) return res.send({ message: 'User not registered' });
+        if (!user) return res.status(400).send({ message: 'User not registered' });
 
         const pass_ok = await bcrypt.compare(password, user.password);
-        if (!pass_ok) return res.send({ message: 'Error to authenticate user' });
+        if (!pass_ok) return res.status(401).send({ message: 'Error to authenticate user' });
 
         user.password = undefined;
         return res.send({ user, token: createTokenUser(user.id) });
 
     } catch (error) {
-        return res.send({ message: 'Error fetching users' });
+        return res.status(500).send({ message: 'Error fetching users' });
     }
 });
 
